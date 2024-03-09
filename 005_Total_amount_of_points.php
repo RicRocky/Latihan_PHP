9eac0baac42412e9d5631ff2f55222507c5bf31c
RicRocky <ryzerogtcah@gmail.com>
2024-03-09 09:40:58 +0800

    Latihan

--- 005_Total_amount_of_points.php
+++ 005_Total_amount_of_points.php
@@ -1,3 +1,13 @@
 <?php
-    
-?>
+    function points(array $games): int {
+        $poin = 0;
+        for ($i = 0; $i < count($games); $i++){
+            $games[$i]
+            if (intval($games[$i][0]) > intval($games[$i][2])){
+                $poin += 3;
+            }else if(intval($games[$i][0]) == intval($games[$i][2])){
+                $poin += 1;
+            }
+        }
+        return $poin;
+    }
